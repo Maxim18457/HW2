b9faa3acd3a5ca98824b9d27ad5df01e9856e4d7
Maxim <filatovmaxim06@gmail.com>
2023-09-17 11:31:41 +0300

    first commit

---
+++ дз 8.py
@@ -0,0 +1,27 @@
+# class Book:
+#     def __init__(self,title,author,year):
+#         self.title = title
+#         self.author = author
+#         self.year = year
+#     def name_title(self):
+#         return self.title
+#     def name_author(self):
+#         return self.author
+#     def get_year(self):
+#         return self.year
+# book_1 = Book('Война и мир','Лев Толстой',1869)
+# book_2 = Book('Преступление и наказание', 'Фёдор Достоевский', 1866)
+# book_3 = Book('Старуха Изергиль', 'Максим Горький', 1894)
+
+# print('Название:', book_1.name_title())
+# print('Автор:', book_1.name_author())
+# print('Год:', book_1.get_year())
+
+# print('Название:', book_2.name_title())
+# print('Автор:', book_2.name_author())
+# print('Год:', book_2.get_year())
+
+# print('Название:', book_3.name_title())
+# print('Автор:', book_3.name_author())
+# print('Год:', book_3.get_year())
+
