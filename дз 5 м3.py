965db318f7524499cb89002b2a1ceaf3841985a9
Maxim18457 <144538061+Maxim18457@users.noreply.github.com>
2023-09-29 19:10:06 +0300

    дз 5 модуль 33

--- дз 5 м3.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-def delete_users_by_name(cursor, user_name):
-    command = '''
-DELETE FROM users WHERE name = ?;
-'''
-    cursor.execute(command,(user_name, ))
-with sqlite3.connect('data.db') as cursor:
-    delete_users_by_name(cursor,'Дмитрий')
