279db1f5a140176d1a02c6439f063d1d5f08eae2
Maxim18457 <144538061+Maxim18457@users.noreply.github.com>
2023-10-22 12:14:50 +0300

    Add files via upload

--- advertisements_2/app_lesson_4/urls.py
+++ advertisements_2/app_lesson_4/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from .views import index, top_sellers
 
+
 urlpatterns = [
     path('', index, name='main-page'),
     path('top-sellers/', top_sellers, name='top-sellers'),
     path('advertisement/', index, name='adv-post'),
 ]
+
+
+
+
 
 
 
